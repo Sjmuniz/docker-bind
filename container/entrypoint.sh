833b7dfc4dbefb04b19c23f66fc073f34047862c
Sebastian Muniz <sebastian.muniz@bairesdev.com>
2019-08-24 00:40:15 -0300

    Issue #26 - Added some logic to create rndc at /root and link it to /etc/bind/

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 OPTIONS=$@
+RNDCFILE=/root/rndc.key
+#Deploy rndc from $RNDCFILE to /etc/bind linking it.
+if test -f "$RNDCFILE"; then
+    rm -vf /etc/bind/rndc.key
+    ln -vs $RNDCFILE /etc/bind/rndc.key
+else
+  echo "$RNDCFILE not found. You might have problems using $(which rndc) to control the daemon. Skipping..."
+fi
 chown -R root:named /etc/bind /var/run/named
 chown -R named:named /var/cache/bind
 chmod -R 770 /var/cache/bind /var/run/named
